ae2eb2634539c263da7f4af50ea7805a467ad864
Nehajamatia <101030433+Nehajamatia@users.noreply.github.com>
2023-03-15 14:27:15 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,9 @@
+//Write a JavaScript function to check whether an `input` is an array or not.
+var is_array = function(input) {
+    if (toString.call(input) === "[object Array]")
+      return true;
+    return false;   
+      };
+  console.log(is_array('source'));
+  console.log(is_array([1, 2, 4, 0]));
+  
